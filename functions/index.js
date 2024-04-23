e81f8a58e87c924825405d7a644329b02fdf67ca
shshsh1385 <133534564+shshsh1385@users.noreply.github.com>
2024-04-23 21:43:31 +0330

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,5 @@
-const doh = 'https://security.cloudflare-dns.com/dns-query'
-const dohjson = 'https://security.cloudflare-dns.com/dns-query'
+const doh = 'https://family.adguard-dns.com/dns-query'
+const dohjson = 'https://family.adguard-dns.com/dns-query'
 const contype = 'application/dns-message'
 const jstontype = 'application/dns-json'
 
